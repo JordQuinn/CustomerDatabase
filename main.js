d0cf4bf4d813cf55ded3e5fa17c512454cfa60f9
Your Name <jordanmichelequinn@gmail.com>
2017-07-12 17:16:57 -0700

    final edits

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 let htmlString = ""
 
 customers.results.forEach(function(customer){
-  htmlString += `<img src="${customer.picture.large}"/>
+  htmlString += `
+<div id = "employee">
+  <img src="${customer.picture.large}"/>
   <h2>${customer.name.first} ${customer.name.last}
+  <hr>
   <h3>${customer.email}
   <p>${customer.location.street}
   <p>${customer.location.city}, ${customer.location.state}  ${customer.location.postcode}
   <p>${customer.phone}
   <div>${customer.id.value}</div>
-
+</div>
 `
 })
 document.querySelector('#container').innerHTML = htmlString
